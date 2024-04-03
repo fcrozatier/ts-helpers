38c868c5a64bf2f0b6deb2ff28a81d5b03ae19b5
fcrozatier <frederic.crozatier@protonmail.com>
2024-04-03 17:18:03 +0200

    set main entrypoint

---
+++ index.ts
@@ -0,0 +1,6 @@
+export * from "./src/arrays";
+export * from "./src/numbers";
+export * from "./src/objects";
+export * from "./src/promises";
+export * from "./src/random";
+export * from "./src/sets";
