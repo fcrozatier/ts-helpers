282757c3006b5a30dec463eef5c8f611e7fc1dde
fcrozatier <frederic.crozatier@protonmail.com>
2024-04-03 16:14:24 +0200

    add isKey type predicate

--- src/objects.ts
+++ src/objects.ts
@@ -4,3 +4,16 @@
 export const keys = Object.keys as <T>(
 	o: T,
 ) => Extract<keyof T, string | number>[];
+
+/**
+ * Type predicate for narrowing a string to be used to index an object
+ *
+ * Example:
+ * Object.keys(user).forEach((key) => {
+		if (isKey(user, key)) {
+			console.log(user[key]);
+			}
+ */
+export function isKey<T extends object>(x: T, k: PropertyKey): k is keyof T {
+	return k in x;
+}
