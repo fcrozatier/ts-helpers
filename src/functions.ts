26c81a1ac4b0b513c5a16977d5c7292ff292a82e
fcrozatier <frederic.crozatier@protonmail.com>
2024-04-04 17:09:31 +0200

    add once decorator

---
+++ src/functions.ts
@@ -0,0 +1,14 @@
+/**
+ * Decorator to make `fn` only callable once
+ */
+// The use of T extends unknown[] instead of ...args: T[] doesn't force all attributes to be of the same type
+export function once<T extends unknown[], U>(fn: (...args: T) => U) {
+	let called = false;
+
+	return function (...args: T) {
+		if (!called) {
+			fn.call(null, ...args);
+			called = true;
+		}
+	};
+}
