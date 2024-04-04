0e340cd52e0ef1fd1c71a79f8246c6ed7bd3a309
fcrozatier <frederic.crozatier@protonmail.com>
2024-04-04 10:00:50 +0200

    include index

--- index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./src/arrays";
-export * from "./src/numbers";
-export * from "./src/objects";
-export * from "./src/promises";
-export * from "./src/random";
-export * from "./src/sets";
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * from "./arrays.js";
+export * from "./numbers.js";
+export * from "./objects.js";
+export * from "./promises.js";
+export * from "./random.js";
+export * from "./sets.js";
