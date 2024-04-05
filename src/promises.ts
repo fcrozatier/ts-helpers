895bcd27fdb14adc597bd7ed2879dcc4c7bf6458
fcrozatier <frederic.crozatier@protonmail.com>
2024-04-05 09:12:47 +0200

    improve debounce types

--- src/promises.ts
+++ src/promises.ts
@@ -4,14 +4,14 @@
  * @param delay - Minimum delay in ms between calls.
  * @param lazy - If lazy then every new call to `func` resets the delay timer. Otherwise the function ensures there is at least `delay` ms between calls
  */
-export function debounce<T, U>(
-	func: (...args: T[]) => U,
+export function debounce<T extends unknown[], U>(
+	func: (...args: T) => U,
 	delay = 100,
 	lazy = true,
 ) {
 	let timeoutID: number | null;
 
-	function debounced(...args: T[]) {
+	function debounced(...args: T) {
 		if (timeoutID && lazy) {
 			clearTimeout(timeoutID);
 		}
