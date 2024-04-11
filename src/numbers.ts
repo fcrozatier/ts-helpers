6fe470f6de1cab2883b49c44314ca0d0b01716a2
fcrozatier <frederic.crozatier@protonmail.com>
2024-04-11 14:28:10 +0200

    add modulo

--- src/numbers.ts
+++ src/numbers.ts
@@ -6,3 +6,19 @@ export function round(value: number, decimalPlaces = 0) {
 	const multiplier = Math.pow(10, decimalPlaces);
 	return Math.round(value * multiplier) / multiplier;
 }
+
+/**
+ * Returns `n` modulo `d`
+ *
+ * The modulo operator has the same sign as the divisor `d` whereas the remainder n % d has
+ * the same size as the dividend `n`
+ *
+ * @example
+ * ```ts
+ * modulo(-1,3); //2
+ * -1 % 3;  //-1
+ * ```
+ */
+export function modulo(n: number, d: number) {
+	return ((n % d) + d) % d;
+}
--- tests/numbers.test.ts
+++ tests/numbers.test.ts
@@ -1,10 +1,27 @@
-import { describe } from "vitest";
+import { describe, test } from "vitest";
 import { expect } from "vitest";
 import { it } from "vitest";
-import { round } from "../src/numbers";
+import { modulo, round } from "../src/numbers";
 
 describe("round", () => {
 	it("rounds to a given precision", () => {
 		expect(round(Math.PI, 2)).toBe(3.14);
+	});
+});
+
+describe("modulo", () => {
+	test.each([
+		[12, 5],
+		[12, 4],
+		[12, 3],
+		[12, 2],
+		[12, 6],
+	])("equals the remainder with positive numbers", (n, d) => {
+		expect(modulo(n, d)).toBe(n % d);
+	});
+
+	it("works with a negative dividend", () => {
+		expect(modulo(-1, 3)).toBe(2);
+		expect(-1 % 3).toBe(-1);
 	});
 });
