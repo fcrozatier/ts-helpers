9ad2f0dc2463fa6646fd518bc969fc26a58327ec
fcrozatier <frederic.crozatier@protonmail.com>
2024-05-06 15:25:41 +0200

    introduce a type function

---
+++ src/type.ts
@@ -0,0 +1,66 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof#custom_method_that_gets_a_more_specific_type
+
+export type LooseAutocomplete<T extends string> = T | Omit<string, T>;
+
+type Types = LooseAutocomplete<
+	| "null"
+	| "undefined"
+	| "boolean"
+	| "number"
+	| "bigint"
+	| "string"
+	| "symbol"
+	| "function"
+	| "class"
+	| "array"
+	| "date"
+	| "error"
+	| "regexp"
+	| "object"
+>;
+
+/**
+ * A more reliable `type` function
+ */
+export function type(value: unknown): Types {
+	if (value === null) {
+		return "null";
+	}
+
+	if (value === undefined) {
+		return "undefined";
+	}
+
+	const baseType = typeof value;
+	// Primitive types
+	if (!["object", "function"].includes(baseType)) {
+		return baseType;
+	}
+
+	// Symbol.toStringTag often specifies the "display name" of the
+	// object's class. It's used in Object.prototype.toString().
+	// @ts-expect-error
+	const tag = value[Symbol.toStringTag];
+	if (typeof tag === "string") {
+		return tag;
+	}
+
+	// If it's a function whose source code starts with the "class" keyword
+	if (
+		baseType === "function" &&
+		Function.prototype.toString.call(value).startsWith("class")
+	) {
+		return "class";
+	}
+
+	// The name of the constructor; for example `Array`, `GeneratorFunction`,
+	// `Number`, `String`, `Boolean` or `MyCustomClass`
+	const className = value.constructor.name;
+	if (typeof className === "string" && className !== "") {
+		return className.toLowerCase();
+	}
+
+	// At this point there's no robust way to get the type of value,
+	// so we use the base implementation.
+	return baseType;
+}
---
+++ tests/types.test.ts
@@ -0,0 +1,34 @@
+import { describe, test } from "vitest";
+import { expect } from "vitest";
+import { it } from "vitest";
+import { type } from "../src/type";
+
+describe("type", () => {
+	it("discriminates types", () => {
+		// null / undefined types
+		expect(type(null)).toBe("null");
+		expect(type(undefined)).toBe("undefined");
+
+		// other primitive types
+		expect(type(0)).toBe("number");
+		expect(type(BigInt(0))).toBe("bigint");
+		expect(type(true)).toBe("boolean");
+		expect(type("")).toBe("string");
+		expect(type(Symbol(""))).toBe("symbol");
+
+		// object types
+		expect(type([])).toBe("array");
+		expect(type(new Date())).toBe("date");
+		expect(type(new Boolean(true))).toBe("boolean");
+		expect(type(new Number(0))).toBe("number");
+		expect(type(new String(""))).toBe("string");
+		expect(type(new Error(""))).toBe("error");
+		expect(type(new RegExp("a"))).toBe("regexp");
+		expect(type(/a/)).toBe("regexp");
+		expect(type({})).toBe("object");
+
+		// function type
+		expect(type(() => 1)).toBe("function");
+		expect(type(class Dog {})).toBe("class");
+	});
+});
