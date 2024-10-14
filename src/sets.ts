b266fe0a6fc53d6a13a860fb2843742d1a591f70
fcrozatier <frederic.crozatier@protonmail.com>
2024-10-14 11:10:04 +0200

    add future implementation

--- src/sets.ts
+++ src/sets.ts
@@ -1,3 +1,5 @@
 export function areSetsEqual(a: Set<unknown>, b: Set<unknown>) {
+	// Not yet mainstream in 2024 (82% support) https://caniuse.com/?search=isSupersetOf
+	// return a.size === b.size && b.isSupersetOf(a);
 	return a.size === b.size && [...b].every((el) => a.has(el));
 }
