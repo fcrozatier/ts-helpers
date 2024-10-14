c7180e6eecba272e041a0155d28793cf55088e01
fcrozatier <frederic.crozatier@protonmail.com>
2024-10-14 11:05:15 +0200

    refactor set equality

--- src/sets.ts
+++ src/sets.ts
@@ -1,7 +1,3 @@
 export function areSetsEqual(a: Set<unknown>, b: Set<unknown>) {
-	if (a.size !== b.size) {
-		return false;
-	}
-
-	return [...b].map((el) => a.has(el)).every(Boolean);
+	return a.size === b.size && [...b].every((el) => a.has(el));
 }
